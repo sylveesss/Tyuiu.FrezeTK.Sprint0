e47ce95312bc5856d62dd1f645c1fa4656adc47c
freze <freze@DESKTOP-CA1MSJQ>
2026-09-15 22:38:26 +0500

    Task0

--- Tyuiu.FrezeTK.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.FrezeTK.Sprint0.Task3.V0/Program.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.FrezeTK.Sprint0.Task2.V0.Lib;
+using Tyuiu.FrezeTK.Sprint0.Task3.V0.Lib;
 
-namespace Tyuiu.FrezeTK.Sprint0.Task2.V0
+namespace Tyuiu.FrezeTK.Sprint0.Task3.V0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.GetMessage("Татьяна"));
+            Console.WriteLine(DataService.Sum(7, 7));
             Console.ReadKey();
         }
     }
