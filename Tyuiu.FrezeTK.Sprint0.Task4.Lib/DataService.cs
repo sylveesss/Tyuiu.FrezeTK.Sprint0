108fa5c2e825aa997aa625bbdc8522572528167c
freze <freze@DESKTOP-CA1MSJQ>
2026-09-15 22:48:45 +0500

    Task4

---
+++ Tyuiu.FrezeTK.Sprint0.Task4.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FrezeTK.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+}
