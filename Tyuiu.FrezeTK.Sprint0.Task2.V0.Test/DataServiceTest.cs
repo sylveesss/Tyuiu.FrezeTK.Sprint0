0cdd81100ca621d664cae7a434470f3e4c048413
freze <freze@DESKTOP-CA1MSJQ>
2026-09-15 21:27:28 +0500

    Task2

---
+++ Tyuiu.FrezeTK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.FrezeTK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.FrezeTK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Татьяна";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Татьяна", res);
+        }
+    }
+}
---
+++ Tyuiu.FrezeTK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.FrezeTK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.FrezeTK.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Татьяна"));
+            Console.ReadKey();
+        }
+    }
+}
